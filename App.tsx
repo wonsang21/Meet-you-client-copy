41f0239782d07dc407da91c11629015f6eddbabf
won <rjsdnql123@gmail.com>
2020-06-15 14:21:24 +0900

    Add TEST App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main/Main'
+import reducers from "./src/reducers/index";
+import { Provider } from "react-redux";
+import Recommend from './src/components/Recommenduser'
+import Capp from './src/containers/Capp';
+// import store from './src/store';
+// import { createStore } from "redux";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Detail from './src/components/userfile/Detail'
+import { navigationRef } from './src/RootNavigation';
+import { createStore, compose } from 'redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+declare global {
+  interface Window {
+    devToolsExtension: typeof compose;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const store = createStore(
+  reducers,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  
+  return (
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator >
+          <Stack.Screen name="Home" component={Capp} />
+          <Stack.Screen name="Details" component={Detail} />
+          <Stack.Screen name="Recommend" component={Recommend} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
