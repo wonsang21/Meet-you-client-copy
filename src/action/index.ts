5cfb3687ce8acbbd0e1e001e577ff51d643ba73e
won <rjsdnql123@gmail.com>
2020-06-16 22:53:15 +0900

    Fix containers Change fakedata to axios

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,12 +1,12 @@
 export const SET_USER = 'SET_USER'
 export const USER_RE = 'USER_RE'
 
-export const setUser = () => ({
+export const setUser = (user:[]) => ({
     type: SET_USER,
-    
+    user
 })
 
 export const UserRE = () => ({
-    type: USER_RE
-
+    type: USER_RE,
+    
 })
--- src/components/userfile/Detail.tsx
+++ src/components/userfile/Detail.tsx
@@ -9,15 +9,19 @@ const StylePhoto = styled.Image`
   height: 300px;
 `;
 const UserNameAge = styled.Text`
-   font-size: 11px
-     
+   font-size: 21px
 `;
-// const Aewrw = styled.Text`
+const UserbloodAndaddress = styled.Text`
+   font-size: 17px
+`;
+const Sadsfe = styled.Text`
+    background-color: mistyrose;
 
-// border-left-width: 1px;
-// border-left-style : solid;
-// border-left-color : #fff
-// `
+    padding: 5px;
+    margin: 10px;
+    border-radius: 50px;
+`;
+
 interface Props{
     route:any
 }
@@ -28,16 +32,24 @@ function DetailsScreen({ route }: Props) {
         <View style={{ flex: 1}}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <StylePhoto source={{ uri: route.params.user.profile_photo}}></StylePhoto>
-                <UserNameAge>이름  :  {route.params.user.username},{route.params.user.age}</UserNameAge>
+                <UserNameAge>{route.params.user.username}, {route.params.user.nickname}, {route.params.user.age}</UserNameAge>
+                <UserbloodAndaddress>{route.params.user.address}, {route.params.user.blood}</UserbloodAndaddress>
+                <Text>{route.params.user.gender}</Text>
+                <Text>{route.params.user.drinking}</Text>
+                <Text>{route.params.user.smoking}</Text>
+                <Text>{route.params.user.job}</Text>
+                <Text>{route.params.user.school}</Text>
         </View>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{route.params.user.address}</Text>
-            <Text>{route.params.user.nickname}</Text>
-            <Text>{route.params.user.blood}</Text>
-            <Text>{route.params.user.gender}</Text>
-            <Text>{route.params.user.drinking}</Text>
-            <Text>{route.params.user.smoking}</Text>
-            <Text>{route.params.user.job}</Text>
+            <Text>  내 취미 는 {route.params.user.hobbies.map((hobby:string,index:number) => 
+                     <Sadsfe key ={index} >{hobby}</Sadsfe>
+                )}</Text>
+                <Text>이상형 {route.params.user.personalities.map((idealType: string, index: number) =>
+                    <Sadsfe key={index}>{idealType}</Sadsfe>
+                )}</Text>
+              <Text> 내성격 {route.params.user.personalities.map((personality: string, index: number) =>
+                    <Sadsfe key={index}>{personality}</Sadsfe>
+                )}</Text>
         </View>
         </View>
     );
