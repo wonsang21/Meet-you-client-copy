5347bc289f305a464ca251810298e8a0f31b7db1
won <rjsdnql123@gmail.com>
2020-06-20 18:53:30 +0900

    Fix Recommend User

--- src/reducers/UserPhoto.ts
+++ src/reducers/UserPhoto.ts
@@ -1,12 +1,12 @@
-import { SET_USER, USER_RE, MY_PRO_FILE } from "../action";
+import { SET_USER, USER_RE, MY_PRO_FILE, OLD_USER, USER_HOBBY, RECENTLY_USER, IDEALTYPE_USER, PERSONALITY_USER } from "../action";
 
 interface AppState{
 
 }
 
-type Action =
-    | { type: "SET_USER"; user: []}
-    | { type: "USER_RE"; user:[] };
+// type Action =
+//     | { type: "SET_USER"; user: []}
+//     | { type: "USER_RE"; user:[] };
 
 const UserPhoto = (state: AppState, action: Action) => {
     console.log(action,'action')
@@ -27,7 +27,26 @@ const UserPhoto = (state: AppState, action: Action) => {
             return Object.assign({}, state, {
                 myprofile: action.profile
             })
-
+        case OLD_USER:
+            return Object.assign({}, state, {
+                odlUser: action.user
+            })
+        case USER_HOBBY:
+            return Object.assign({}, state, {
+                userHobby: action.user
+            })
+        case RECENTLY_USER:
+            return Object.assign({},state,{
+                recentlyUser:action.user
+            })
+        case IDEALTYPE_USER:
+            return Object.assign({}, state, {
+                idealTypeUser: action.user
+            })
+        case PERSONALITY_USER:
+            return Object.assign({}, state, {
+                personalityUser: action.user
+            })
     }
     
 }
